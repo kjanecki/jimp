aec1ef0e6bd05d667de0fcd36b4cafd9345b1283
Krzysiek <janekjanecki14@gmail.com>
2017-03-12 21:09:29 +0100

    lab2 - Greatest Product; ukonczony krok 1 2;

---
+++ lab2/greatestproduct/GreatestProduct.h
@@ -0,0 +1,12 @@
+//
+// Created by Krzysiek on 12.03.2017.
+//
+#include <vector>
+#ifndef JIMP_EXERCISES_GREATESTPRODUCT_H
+#define JIMP_EXERCISES_GREATESTPRODUCT_H
+
+
+int GreatestProduct(const std::vector<int> &numbers, int k);
+
+
+#endif //JIMP_EXERCISES_GREATESTPRODUCT_H

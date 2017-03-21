b718845f36784f4e2e3155c3bc701063ec155cbf
Krzysiek <janekjanecki14@gmail.com>
2017-03-21 09:45:28 +0100

    lab3 - tinyurl

---
+++ lab3/tinyurl/TinyUrl.h
@@ -0,0 +1,32 @@
+//
+// Created by Krzysiek on 20.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_TINYURL_H
+#define JIMP_EXERCISES_TINYURL_H
+
+#include <utility>
+#include <string>
+#include <array>
+#include <memory>
+#include <map>
+
+namespace tinyurl {
+
+    struct TinyUrlCodec{
+        std::string code;
+        std::string url;
+    };
+
+
+
+    std::unique_ptr<TinyUrlCodec> Init();
+
+    void NextHash(std::array<char, 6> *state);
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+
+}
+#endif //JIMP_EXERCISES_TINYURL_H
---
+++ lab3/tinyurl/main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Krzysiek on 20.03.2017.
+//
+

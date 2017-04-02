bc94a6ef00e460ae06169ffb5e42d229d8e1597e
Krzysiek <janekjanecki14@gmail.com>
2017-04-02 19:35:47 +0200

    Merge branch 'master' of https://gitlab.com/agh-jimp2/exercises into lab5
    # Conflicts:
    #	lab5/CMakeLists.txt
    
    Lab5 - TextPool final

--- lab5/textpool/TextPool.h
+++ lab5/textpool/TextPool.h
@@ -9,11 +9,36 @@
 #include <string>
 #include <experimental/string_view>
 #include <set>
+#include <cstring>
+#include <algorithm>
+
 
 namespace pool{
 
     class TextPool{
+    public:
+        TextPool() {
+            //StringSet_= new std::set<std::experimental::string_view>;
+        }
 
+        TextPool(const std::initializer_list<std::experimental::string_view> &elements);
+
+        TextPool(const TextPool &text) = delete;
+
+        TextPool &operator=(const TextPool &text) = delete;
+
+        TextPool( TextPool &&text);
+
+        TextPool &operator=(TextPool &&text);
+
+        ~TextPool();
+
+        std::experimental::string_view Intern(const std::string &str);
+
+        size_t StoredStringCount() const;
+
+    private:
+        std::set<std::experimental::string_view> StringSet_;
     };
 }
 

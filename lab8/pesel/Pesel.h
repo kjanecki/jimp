6ba8424b1ffd36360cf5bf3c30f46e73522695c0
Krzysiek <janekjanecki14@gmail.com>
2017-04-27 15:39:34 +0200

    Lab8 - Pesel

--- lab8/pesel/Pesel.h
+++ lab8/pesel/Pesel.h
@@ -5,18 +5,46 @@
 #ifndef JIMP_EXERCISES_PESEL_H
 #define JIMP_EXERCISES_PESEL_H
 
+#include <iostream>
+#include <stdexcept>
+
 namespace academia{
+
+    class AcademiaDataValidationError:public std::runtime_error{
+    public:
+        AcademiaDataValidationError(const std::string &__arg):std::runtime_error(__arg),str_(__arg){};
+        virtual ~AcademiaDataValidationError(){};
+
+        std::string str_;
+    };
+
+    class InvalidPeselLength:public AcademiaDataValidationError{
+    public:
+
+        InvalidPeselLength(const std::string &str, int length):AcademiaDataValidationError(str){};
+    };
+
+    class InvalidPeselChecksum:public AcademiaDataValidationError{
+    public:
+        InvalidPeselChecksum(const std::string &str, int sum):AcademiaDataValidationError(str){};
+    };
+
+    class InvalidPeselCharacter:public AcademiaDataValidationError{
+    public:
+        InvalidPeselCharacter(const std::string &str):AcademiaDataValidationError(str){};
+    };
+
 
     class Pesel{
     public:
         Pesel()= delete;
-        Pesel(const char *c):c_(c){
-
+        Pesel(const std::string &str):pesel_(str){
+            ValidatePESEL(pesel_);
         };
-        void ValidatePesel(const char *c);
-
-
-        const char*c_;
+        std::string GetString(){return pesel_;};
+    private:
+        void ValidatePESEL(const std::string &str);
+        std::string pesel_;
     };
 
 }
--- lab8/pesel/main.cpp
+++ lab8/pesel/main.cpp
@@ -5,8 +5,19 @@
 #include <iostream>
 #include "Pesel.h"
 
-
+using namespace academia;
 int main(){
+
+    try{
+        Pesel p{"44051401359"};
+    }catch(const InvalidPeselChecksum &arg){
+        std::cerr << arg.what() << "\n";
+    }catch(const InvalidPeselLength &arg){
+        std::cerr << arg.what() << "\n";
+    }catch(const InvalidPeselCharacter &arg){
+        std::cerr << arg.what() << "\n";
+    }
+
 
     return 0;
 }
